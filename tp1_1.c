731e0c24e95a5d5b3c66cca5304459d3427db5c0
AgustinJNadal <nadaljorge32@gmail.com>
2025-04-01 01:57:53 -0300

    ignorado vscode y escribi los printf que pedia

--- tp1_1.c
+++ tp1_1.c
@@ -3,6 +3,12 @@
 int main() {
     int s = 12;
     int *puntero = &s;
-    
+
+    printf("El contenido del puntero: %d\n", *puntero);
+    printf("Direccion de memoria almacenada por el puntero: %p\n", puntero);
+    printf("Direccion de memoria de la variable: %p\n", &s);
+    printf("Direccion de memoria del puntero: %p\n", &puntero);
+    printf("Tamanio de memoria utilizado por la variable: %d bytes\n", sizeof(s));
+
     return 0;
 }
