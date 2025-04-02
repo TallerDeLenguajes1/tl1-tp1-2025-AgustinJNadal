14e83d36129ba9d3f6c4b8ad706061efc0f3abee
AgustinJNadal <nadaljorge32@gmail.com>
2025-04-01 21:39:20 -0300

    devolviendo tipo void

--- tp1_2.c
+++ tp1_2.c
@@ -1,20 +1,16 @@
 #include <stdio.h>
 
-int cuadradoNum(int num);
+void cuadradoNum(int num);
 
 int main() {
-    int num = 19;
-    int cuadrado;
-
-    cuadrado = cuadradoNum(num);
-
-    printf("El cuadrado de %d es: %d\n", num, cuadrado);
+    int num = 12;
+    cuadradoNum(num);
 
     return 0;
 }
 
-int cuadradoNum(int num){
+void cuadradoNum(int num){
     int cuadrado;
     cuadrado = num * num;
-    return cuadrado;
+    printf("El cuadrado de %d es: %d\n", num, cuadrado);
 }
